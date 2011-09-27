38c56deab2baa7b2f8bd90ea6270e981b957a88f
Sandor Szuecs <sandor.szuecs@fu-berlin.de>
2011-09-27 17:55:54 +0200

    use sandbox to disallow network connections

--- rb_main.rb
+++ rb_main.rb
@@ -1,15 +1,20 @@
 framework 'Cocoa'
 
-# Loading all the Ruby project files.
-main = File.basename(__FILE__, File.extname(__FILE__))
-dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
-Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
-  if path != main
-    require(path)
+begin
+  Sandbox.no_internet.apply!
+  # Loading all the Ruby project files.
+  main = File.basename(__FILE__, File.extname(__FILE__))
+  dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
+  Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
+    if path != main
+      require(path)
+    end
   end
-end
 
-# where do I get the frame from?
-#drag_drop  = DropView.alloc.initWithFrame( $Frame )
-# Starting the Cocoa main loop.
-NSApplicationMain(0, nil)
+  # where do I get the frame from?
+  #drag_drop  = DropView.alloc.initWithFrame( $Frame )
+  # Starting the Cocoa main loop.
+  NSApplicationMain(0, nil)
+rescue SystemCallError => exception
+  puts exception
+end
